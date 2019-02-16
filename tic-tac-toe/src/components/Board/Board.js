a2218a817cd675bfd8fc4bbcda58516622880f69
Sercan Leylek <sercan@ravn.no>
2019-02-16 15:50:39 +0100

    Kick-off tic-tac-toe game with react

---
+++ tic-tac-toe/src/components/Board/Board.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Square from '../Square/Square';
+
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true
+        }
+    }
+
+    renderSquare(i) {
+        return (<Square
+                        value={this.state.squares[i]}
+                        onClick={() => this.handleClick(i)}
+                />
+        );
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+    render() {
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(9)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Game from './components/Game/Game';
+
+ReactDOM.render(
+<Game />,
+    document.getElementById('root')
+);
