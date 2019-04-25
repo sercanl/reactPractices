ca610af83f5beb5ad61f283da7228f8e1ec1543b
Sercan Leylek <mr.stork@gmail.com>
2019-04-25 14:46:20 +0200

    Update fire.js

--- show-cities-firebase/src/fire.js
+++ show-cities-firebase/src/fire.js
@@ -3,7 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 let config = {
-    apiKey: "AIzaSyA4ixj7ItE70jzJkAMB_BD7HBYpIn7nG3g",
+    apiKey: "",
     authDomain: "cities-62a97.firebaseapp.com",
     databaseURL: "https://cities-62a97.firebaseio.com",
     projectId: "cities-62a97",
